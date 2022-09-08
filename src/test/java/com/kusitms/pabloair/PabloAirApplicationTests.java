a3ee5a6400b18c06104a7c2e925ed098897a1f49
yujeongkimm <20201111@duksung.ac.kr>
2022-09-08 14:21:43 +0900

    init

---
+++ src/test/java/com/kusitms/pabloair/PabloAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.pabloair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabloAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
