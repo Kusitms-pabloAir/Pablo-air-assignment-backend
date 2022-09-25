be94ae3b87c994229eb42312cc824e089e06ff28
mingeun0507 <mkkim999@gmail.com>
2022-09-25 20:01:40 +0900

    Feat: AES256을 사용한 암호화로 변경

--- src/main/java/com/kusitms/pabloair/service/ValidateService.java
+++ src/main/java/com/kusitms/pabloair/service/ValidateService.java
@@ -1,23 +1,22 @@
 package com.kusitms.pabloair.service;
 
-import com.kusitms.pabloair.domain.Order;
+import com.kusitms.pabloair.config.AES256Util;
 import com.kusitms.pabloair.domain.User;
 import com.kusitms.pabloair.repository.UserRepository;
 import com.kusitms.pabloair.repository.ValidateRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ValidateService {
 
-    private final PasswordEncoder passwordEncoder;
+    // private final PasswordEncoder passwordEncoder;
+    private final AES256Util aes256Util;
     private final ValidateRepository validateRepository;
     private final UserRepository userRepository;
 
@@ -40,10 +39,19 @@ public class ValidateService {
         serialNumber = sdf.format(dateTime.getTime());
 
         //yyyymmddhh24missSSS_userid_랜덤문자6개
-        serialNumber = serialNumber + "_"  + userId + "_" + RandomStringUtils.randomAlphanumeric(6);
+        serialNumber = serialNumber + "_" + userId + "_" + RandomStringUtils.randomAlphanumeric(6);
 
         // 암호화
-        String encode = passwordEncoder.encode(serialNumber);
+        // String encode = passwordEncoder.encode(serialNumber);
+
+        //AES256 사용 암호화
+        String encode;
+
+        try {
+            encode = aes256Util.encrypt(serialNumber);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         userRepository.saveWithSerialNumber(userId, encode);
 
